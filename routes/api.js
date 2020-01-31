4fc8584aede6028df3c45fa4d37159fa19294d2a
schwarz13style@gmail.com <schwarz13style@gmail.com>
2020-01-31 21:15:03 +0900

    api.js

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('api', { title: 'This API Page' });
+  res.send('This API Page');
 });
 
 module.exports = router;
